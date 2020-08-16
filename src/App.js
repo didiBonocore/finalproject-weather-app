d1d407bc929202a26d7a24fc8fb11b326f7ff44d
Eugenia Bonocore <eugeniabonocore@gmail.com>
2020-08-16 22:39:08 +0100

    Update footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
+     <footer>
+       Created by Eugenia Bonocore and{""}
+      <a href="https://github.com/didiBonocore/finalproject-weather-app" target="_blank">
+      open-sourced on GitHub
+      </a>
+     </footer>
     </div>
   );
 }
